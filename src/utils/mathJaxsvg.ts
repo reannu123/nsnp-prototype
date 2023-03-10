2554da1aac27968724dc70e6dbecd262d2aef03a
reannu123 <reannumon123@gmail.com>
2023-03-10 21:21:59 +0800

    Fixed ts error in mathjaxsvg.ts

--- src/utils/mathJaxsvg.ts
+++ src/utils/mathJaxsvg.ts
@@ -1,12 +1,7 @@
 import { mathjax } from "mathjax-full/js/mathjax";
 import { TeX } from "mathjax-full/js/input/tex";
 import { SVG } from "mathjax-full/js/output/svg";
-import { LiteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor";
-import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html";
 import { AllPackages } from "mathjax-full/js/input/tex/AllPackages";
-
-const adaptor = new LiteAdaptor();
-RegisterHTMLHandler(adaptor);
 
 const html = mathjax.document("", {
   InputJax: new TeX({ packages: AllPackages }),
@@ -15,9 +10,7 @@ const html = mathjax.document("", {
 
 export function mathJaxSvg(expression: string) {
   const node = html.convert(expression, { display: true });
-  console.log("Node: ", node);
-  const svgOut = adaptor.firstChild(node).outerHTML;
-  console.log("SVG: ", svgOut);
+  const svgOut = node.innerHTML;
   const svgUrl = encodeURI("data:image/svg+xml;utf-8," + svgOut);
   return svgUrl;
 }
